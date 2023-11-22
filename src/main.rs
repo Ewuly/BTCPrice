fa414d4fba390397a67549842af07a47994a1f3d
Ewan <ewan.hamon@gmail.com>
2023-11-22 21:33:10 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::thread;
 use std::time::Duration;
 use std::io::{self, Read};
 
-//main
-//Te
 fn main() {
+    let mut previous_price: f64 = 0.0;
+
     loop {
         let curl_output = Command::new("curl")
             .args(&["-s", "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"])
@@ -21,9 +21,19 @@ fn main() {
             .output()
             .expect("Failed to execute jq command");
 
-        let price = String::from_utf8_lossy(&jq_output.stdout);
-        print!("{}", price);
+        let current_price = String::from_utf8_lossy(&jq_output.stdout);
+        let current_price: f64 = current_price.trim().parse().expect("Failed to parse price as f64");
 
-        thread::sleep(Duration::from_secs(1));
+        if previous_price != 0.0 {
+            let price_change = current_price - previous_price;
+            let percentage_change = (price_change / previous_price) * 100.0;
+
+            //println!("Price Change: {:.6} USD", price_change);
+            println!("Percentage Change: {:.6}%", percentage_change);
+        }
+
+        previous_price = current_price;
+
+        thread::sleep(Duration::from_secs(10));
     }
 }
--- target/debug/BTCPrice.d
+++ target/debug/BTCPrice.d
@@ -1,1 +1,1 @@
-C:\Users\ewanh\..Utiles\Cours\A4\ Fintech\Rust\BTCPrices\target\debug\BTCPrice.exe: C:\Users\ewanh\..Utiles\Cours\A4\ Fintech\Rust\BTCPrices\src\main.rs
+/mnt/c/Users/ewanh/..Utiles/Cours/A4\ Fintech/Rust/BTCPrices/target/debug/BTCPrice: /mnt/c/Users/ewanh/..Utiles/Cours/A4\ Fintech/Rust/BTCPrices/src/main.rs
